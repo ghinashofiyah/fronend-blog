bc67736b61e75025474f55b68c2efc0d18fd4808
anisaurdatunnafiah <anisaurdatunnafiah@gmail.com>
2026-01-27 10:16:57 +0700

    mengganti web

--- frontend/routes/web.php
+++ frontend/routes/web.php
@@ -8,16 +8,15 @@ use App\Http\Controllers\IklanController;
 use App\Http\Controllers\EJurnalController;
 use App\Http\Controllers\AdminController;
 
-// Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::view('/','pages.Dashboard');
 Route::view('/blog/list','pages.Listblog');
-Route::view('/blog/tambah','pages.Tambah');
+Route::view('/blog/tambah','pages.Tambahblog');
 Route::view('/kategori','pages.Kategori');
 Route::view('/iklan','pages.Iklan');
 Route::view('/ejurnal','pages.Ejurnal');
 Route::view('/admin','pages.Admin');
 
-
+// Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 // Route::get('/blog/tambah', [BlogController::class, 'create'])->name('blog.tambah');
 // Route::get('/blog/list', [BlogController::class, 'index'])->name('blog.list');
 // Route::post('/blog', [BlogController::class, 'store'])->name('blog.store');
